e8584ba8457282f5a7e6a152147290384e2f9924
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-02-03 18:24:21 +0100

    installed react-icons

--- Personal-Task-Tracker/src/Components/Task.tsx
+++ Personal-Task-Tracker/src/Components/Task.tsx
@@ -1,10 +1,12 @@
 import { TaskProps } from "../types";
+import { TiDeleteOutline } from "react-icons/ti";
 const Task = ({ title, description, category }: TaskProps) => {
   return (
     <div>
       <h1>{title}</h1>
       <h3>{category}</h3>
       <h4>{description}</h4>
+      <TiDeleteOutline />
     </div>
   );
 };
