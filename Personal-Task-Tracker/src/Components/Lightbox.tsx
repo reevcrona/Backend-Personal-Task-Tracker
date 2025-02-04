ceeb96323114cbc4511ea20566cea50ae0f4d9e6
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-02-04 22:07:38 +0100

    added the prettier tailwind plugin

--- Personal-Task-Tracker/src/Components/Lightbox.tsx
+++ Personal-Task-Tracker/src/Components/Lightbox.tsx
@@ -2,7 +2,11 @@ import "./Lightbox.css";
 import { LightboxProps } from "../types";
 
 const Lightbox = ({ children }: LightboxProps) => {
-  return <div className="light-box">{children}</div>;
+  return (
+    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-black bg-opacity-80">
+      {children}
+    </div>
+  );
 };
 
 export default Lightbox;
